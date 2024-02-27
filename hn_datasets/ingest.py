8565fc22c3fd4499c8ff93c913b7640b0fd999fc
densumesh <dens.sumesh79@gmail.com>
2024-02-27 15:44:57 -0700

    fix ingest script

--- hn_datasets/ingest.py
+++ hn_datasets/ingest.py
@@ -1,11 +1,5 @@
 import ast
-import csv
-import json
-import requests
-import csv
-from tqdm import tqdm
 import redis
-import os
 from trieve_client import AuthenticatedClient
 from trieve_client.models import CreateChunkData, ReturnCreatedChunk, ErrorResponseBody
 from trieve_client.api.chunk import create_chunk
@@ -54,7 +48,6 @@ def ingest_hn():
             and ("title" in item or "text" in item)
             and "deleted" not in item
             and "dead" not in item
-            and item["type"] == "story"
         ):
             print(item.get("id"))
             row = {
@@ -92,67 +85,17 @@ def ingest_hn():
                 time_stamp=datetime.fromtimestamp(row.get("time")).strftime(
                     "%Y-%m-%d %H:%M:%S"
                 ),
+                weight=int(row.get("score")),
             )
             chunk_response = create_chunk.sync(
-                tr_dataset="6d920a1c-fdae-441e-84a5-a9b15f29dd3f",
+                tr_dataset="a1f7188b-7be1-4f6b-8600-68fccd48402f",
                 client=trieve_client,
                 body=data,
             )
             if type(chunk_response) == ErrorResponseBody:
                 print(f"Failed {chunk_response.message}")
                 exit(1)
-        elif (
-            item
-            and "type" in item
-            and "deleted" not in item
-            and "dead" not in item
-            and item["type"] == "comment"
-        ):
-            print(item.get("id"))
-            row = {
-                "by": item.get("by"),
-                "descendants": item.get("descendants"),
-                "id": item.get("id"),
-                "kids": item.get("kids"),
-                "score": item.get("score"),
-                "time": item.get("time"),
-                "title": item.get("title"),
-                "text": (
-                    item.get("text").strip().replace("\n", " ").replace("\r", " ")
-                    if item.get("text")
-                    else None
-                ),
-                "type": item.get("type"),
-                "url": item.get("url"),
-            }
-
-            data = CreateChunkData(
-                chunk_html=row["text"],
-                link=row["url"],
-                metadata={
-                    "by": row.get("by"),
-                    "descendants": row.get("descendants"),
-                    "id": row.get("id"),
-                    "kids": row.get("kids"),
-                    "score": row.get("score"),
-                    "time": row.get("time"),
-                    "title": row.get("title"),
-                    "text": row.get("text"),
-                    "type": row.get("type"),
-                },
-                tracking_id=str(row.get("id")),
-                time_stamp=datetime.fromtimestamp(row.get("time")).strftime(
-                    "%Y-%m-%d %H:%M:%S"
-                ),
-            )
-            chunk_response = create_chunk.sync(
-                tr_dataset="31874d43-9755-4271-8628-c8a33f4f17a4",
-                client=trieve_client,
-                body=data,
-            )
-            if type(chunk_response) == ErrorResponseBody:
-                print(f"Failed {chunk_response.message}")
-                exit(1)
+    
 
 
 ingest_hn()
