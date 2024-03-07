b3564ca497359d7fae1810f1b9caca6180fdcfd2
Dens Sumesh <dens.sumesh79@gmail.com>
2024-03-06 17:19:54 -0800

    feature: updated scirpts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,52 @@
 module.exports = {
-  apps : [{
-    name   : "Get Dataset",
-    script : "./hn_datasets/get_dataset.py",
-    interpreter: "/usr/bin/python",
-    instances: 2
-  },
-  {
-    name   : "app1",
-    script : "./hn_datasets/ingest.py",
-    interpreter: "/usr/bin/python",
-    instances: 5
-  }]
-}
+  apps: [
+    {
+      name: "Get Dataset",
+      script: "./hn_datasets/get_dataset.py",
+      interpreter: "/usr/bin/python3.9",
+      instances: 1,
+      args: "1",
+    },
+    {
+      name: "Get Dataset",
+      script: "./hn_datasets/get_dataset.py",
+      interpreter: "/usr/bin/python3.9",
+      instances: 1,
+      args: "2",
+    },
+    {
+      name: "Get Dataset",
+      script: "./hn_datasets/get_dataset.py",
+      interpreter: "/usr/bin/python3.9",
+      instances: 1,
+      args: "3",
+    },
+    {
+      name: "Get Dataset",
+      script: "./hn_datasets/get_dataset.py",
+      interpreter: "/usr/bin/python3.9",
+      instances: 1,
+      args: "4",
+    },
+    {
+      name: "Get Dataset",
+      script: "./hn_datasets/get_dataset.py",
+      interpreter: "/usr/bin/python3.9",
+      instances: 1,
+      args: "5",
+    },
+    {
+      name: "Get Dataset",
+      script: "./hn_datasets/get_dataset.py",
+      interpreter: "/usr/bin/python3.9",
+      instances: 1,
+      args: "6",
+    },
+    {
+      name: "app1",
+      script: "./hn_datasets/ingest.py",
+      interpreter: "/usr/bin/python3.9",
+      instances: 10,
+    },
+  ],
+};
--- hn_datasets/get_dataset.py
+++ hn_datasets/get_dataset.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import requests
 import redis
@@ -25,33 +26,89 @@ header = [
 
 
 def ingest_hn():
-    final = int(
-        requests.get("https://hacker-news.firebaseio.com/v0/maxitem.json").json()
-    )
+    end = int(requests.get("https://hacker-news.firebaseio.com/v0/maxitem.json").json())
 
-    last_final = redis_client.get("last_final")
+    start = 0
 
-    if last_final == None or final > int(last_final):
-        redis_client.set("last_final", final)
-        start = int(last_final) if last_final != None else 0
+    if sys.argv[1] == "1":
+        start = (end // 6) * 5
+    elif sys.argv[1] == "2":
+        start = (end // 6) * 4
+        end = (end // 6) * 5
+    elif sys.argv[1] == "3":
+        start = (end // 6) * 3
+        end = (end // 6) * 4
+    elif sys.argv[1] == "4":
+        start = (end // 6) * 2
+        end = (end // 6) * 3
+    elif sys.argv[1] == "5":
+        start = (end // 6) * 1
+        end = (end // 6) * 2
+    elif sys.argv[1] == "6":
+        start = 0
+        end = (end // 6) * 1
 
-        item = requests.get(
-            f"https://hacker-news.firebaseio.com/v0/item/{final}.json"
-        ).json()
+    item = requests.get(
+        f"https://hacker-news.firebaseio.com/v0/item/{start}.json"
+    ).json()
 
-        yesterday = datetime.timestamp(datetime.today() - timedelta(days=1))
-        while item["time"] > yesterday and start < final:
-            print(item["id"])
+    if "reversed" in sys.argv:
+        start, end = end, start
+        while end <= start and item is not None:
             if (
-                ("title" in item or "text" in item)
-                and "deleted" not in item
-                and "dead" not in item
+                requests.get(
+                    f"https://api.trieve.ai/api/chunk/tracking_id/{end}",
+                    headers={
+                        "TR-Dataset": "2c3e9673-b685-44fd-a1a2-4d9848c30541",
+                        "Authorization": "tr-6b3fb3MXYs799XuAryTfGzJZmGffN6S3",
+                    },
+                )
+                .json()
+                .get("message")
+                != None
             ):
-                redis_client.lpush("hn", str(item))
-            final -= 1
-            item = requests.get(
-                f"https://hacker-news.firebaseio.com/v0/item/{final}.json"
-            ).json()
+                print(item["id"])
+                if (
+                    ("title" in item or "text" in item)
+                    and "deleted" not in item
+                    and "dead" not in item
+                ):
+                    redis_client.lpush("hn", str(item))
+                end -= 1
+                item = requests.get(
+                    f"https://hacker-news.firebaseio.com/v0/item/{end}.json"
+                ).json()
+    else:
+        while start <= end and item is not None:
+            if (
+                requests.get(
+                    f"https://api.trieve.ai/api/chunk/tracking_id/{start}",
+                    headers={
+                        "TR-Dataset": "2c3e9673-b685-44fd-a1a2-4d9848c30541",
+                        "Authorization": "tr-6b3fb3MXYs799XuAryTfGzJZmGffN6S3",
+                    },
+                )
+                .json()
+                .get("message")
+                != None
+            ):
+                print(item["id"])
+                if (
+                    ("title" in item or "text" in item)
+                    and "deleted" not in item
+                    and "dead" not in item
+                ):
+                    redis_client.lpush("hn", str(item))
+                start += 1
+                item = requests.get(
+                    f"https://hacker-news.firebaseio.com/v0/item/{start}.json"
+                ).json()
+
+    # last_final = redis_client.get("last_final")
+
+    # if last_final == None or final > int(last_final):
+    #     redis_client.set("last_final", final)
+    #     start = int(last_final) if last_final != None else 0
 
 
 while True:
