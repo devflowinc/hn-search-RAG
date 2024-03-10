610aa0684c0b33ca45c8eae2f2c96579c943631e
fastfist <fdenzell@gmail.com>
2024-03-10 07:47:59 -0700

    make it so that it won't fail pinging for an id

--- hn_datasets/get_dataset.py
+++ hn_datasets/get_dataset.py
@@ -13,11 +13,14 @@ redis_client = redis.Redis(
 while True:
     start = redis_client.blpop("tovisit")
     start = int(start[1])
-    item = requests.get(
-        f"https://hacker-news.firebaseio.com/v0/item/{start}.json"
-    ).json()
-    print(item)
+    fail = False
+    try:
+        item = requests.get(
+            f"https://hacker-news.firebaseio.com/v0/item/{start}.json"
+        ).json()
+    except: 
+        fail = True
 
-    if ((item is not None) and ("deleted" not in item) and ("dead" not in item)):
+    if (not fail and (item is not None) and ("deleted" not in item) and ("dead" not in item)):
         print(start)
         redis_client.lpush("hn", str(item))
