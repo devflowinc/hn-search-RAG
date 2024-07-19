f2b49bf1fe442613b31f7ca143c46122efb36253
Dens Sumesh <dens.sumesh79@gmail.com>
2024-07-19 15:33:10 -0700

    feature: use scroll api for home page

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -3,9 +3,9 @@ import Filters from "../components/search/Filters";
 import Header from "../components/Header";
 import { Story } from "../components/search/Story";
 import {
+  ChunkMetadataStringTagSet,
   dateRangeSwitch,
   getFilters,
-  HNStory,
   SearchChunkQueryResponseBody,
   SearchOptions,
 } from "../types";
@@ -46,19 +46,19 @@ export const SearchPage = () => {
   let abortController: AbortController | null = null;
 
   const [selectedDataset, setSelectedDataset] = createSignal(
-    urlParams.get("dataset") ?? "all"
+    urlParams.get("dataset") ?? "all",
   );
   const [sortBy, setSortBy] = createSignal(
-    urlParams.get("sortby") ?? "Relevance"
+    urlParams.get("sortby") ?? "Relevance",
   );
   const [dateRange, setDateRange] = createSignal<string>(
-    urlParams.get("dateRange") ?? "all"
+    urlParams.get("dateRange") ?? "all",
   );
   const [stories, setStories] = createSignal<Story[]>([]);
   const [loading, setLoading] = createSignal(true);
   const [query, setQuery] = createSignal(urlParams.get("q") ?? "");
   const [searchType, setSearchType] = createSignal(
-    urlParams.get("searchType") ?? "fulltext"
+    urlParams.get("searchType") ?? "fulltext",
   );
   const [page, setPage] = createSignal(Number(urlParams.get("page") ?? "1"));
   const [algoliaLink, setAlgoliaLink] = createSignal("");
@@ -88,28 +88,28 @@ export const SearchPage = () => {
     searchOptions.scoreThreshold &&
       urlParams.set(
         "score_threshold",
-        searchOptions.scoreThreshold?.toString()
+        searchOptions.scoreThreshold?.toString(),
       );
 
     urlParams.set("page_size", searchOptions.pageSize.toString());
     urlParams.set(
       "highlight_delimiters",
-      searchOptions.highlightDelimiters.join(",")
+      searchOptions.highlightDelimiters.join(","),
     );
     urlParams.set(
       "highlight_max_length",
-      searchOptions.highlightMaxLength.toString()
+      searchOptions.highlightMaxLength.toString(),
     );
     urlParams.set(
       "highlight_max_num",
-      searchOptions.highlightMaxNum.toString()
+      searchOptions.highlightMaxNum.toString(),
     );
     urlParams.set("highlight_window", searchOptions.highlightWindow.toString());
     urlParams.set("recency_bias", searchOptions.recencyBias.toString());
     urlParams.set("slim_chunks", searchOptions.slimChunks ? "true" : "false");
     urlParams.set(
       "highlight_results",
-      searchOptions.highlightResults ? "true" : "false"
+      searchOptions.highlightResults ? "true" : "false",
     );
 
     if (abortController) {
@@ -119,41 +119,6 @@ export const SearchPage = () => {
     abortController = new AbortController();
     const { signal } = abortController;
 
-    if (query() === "") {
-      fetch(
-        "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty",
-        { signal }
-      )
-        .then((res) => res.json() as Promise<number[]>)
-        .then((storyIds) => {
-          const topStoryIds = storyIds.slice(0, 30);
-          const storyDetails = Promise.all(
-            topStoryIds.map((id: number) =>
-              fetch(
-                `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`,
-                { signal }
-              ).then((res) => res.json() as Promise<HNStory>)
-            )
-          );
-          return storyDetails;
-        })
-        .then((storyDetails) => {
-          const stories: Story[] = storyDetails.map((story) => ({
-            content: story.title,
-            url: story.url,
-            points: story.score,
-            user: story.by,
-            time: new Date(story.time * 1000).toUTCString(),
-            commentsCount: story.descendants,
-            type: story.type,
-            id: story.id,
-          }));
-          setStories(stories);
-        })
-        .catch(() => console.log(""))
-        .finally(() => setLoading(false));
-    }
-
     urlParams.set("q", query());
     urlParams.set("dataset", selectedDataset());
     urlParams.set("sortby", sortBy());
@@ -162,16 +127,16 @@ export const SearchPage = () => {
     urlParams.set("page", page().toString());
     setAlgoliaLink(
       `https://hn.algolia.com/?q=${encodeURIComponent(
-        query()
+        query(),
       )}&dateRange=${dateRange()}&sort=by${
         sortBy() == "Relevance" ? "Popularity" : sortBy()
-      }&type=${selectedDataset()}&page=0&prefix=false`
+      }&type=${selectedDataset()}&page=0&prefix=false`,
     );
 
     window.history.replaceState(
       {},
       "",
-      `${window.location.pathname}?${urlParams.toString()}`
+      `${window.location.pathname}?${urlParams.toString()}`,
     );
 
     const time_range = dateRangeSwitch(dateRange());
@@ -183,6 +148,68 @@ export const SearchPage = () => {
       sort_by_field = "num_value";
     } else {
       sort_by_field = undefined;
+    }
+
+    if (query() === "") {
+      fetch(`${trieveBaseURL}/chunks/scroll`, {
+        method: "POST",
+        body: JSON.stringify({
+          query: query(),
+          search_type: searchType(),
+          page: page(),
+          highlight_results: searchOptions.highlightResults,
+          highlight_delimiters: searchOptions.highlightDelimiters,
+          highlight_max_num: searchOptions.highlightMaxNum,
+          highlight_window: searchOptions.highlightWindow,
+          highlight_max_length: searchOptions.highlightMaxLength,
+          use_weights: sortBy() != "Relevance",
+          sort_by: sort_by_field
+            ? {
+                field: sort_by_field,
+                order: "desc",
+              }
+            : undefined,
+          slim_chunks: searchOptions.slimChunks,
+          filters: getFilters(selectedDataset(), time_range),
+          page_size: searchOptions.pageSize,
+          score_threshold: searchOptions.scoreThreshold,
+        }),
+        headers: {
+          "X-API-VERSION": "V2",
+          "Content-Type": "application/json",
+          "TR-Dataset": trieveDatasetId,
+          Authorization: trieveApiKey,
+        },
+        signal,
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((data: { chunks: ChunkMetadataStringTagSet[] }) => {
+          const stories: Story[] =
+            data.chunks.map((score_chunk): Story => {
+              const story = score_chunk;
+              return {
+                content: story.chunk_html ?? "",
+                url: story.link ?? "",
+                points: story.metadata?.score ?? 0,
+                user: story.metadata?.by ?? "",
+                time: story.time_stamp ?? "",
+                commentsCount: story.metadata?.descendants ?? 0,
+                type: story.metadata?.type ?? "",
+                id: story.tracking_id ?? "0",
+              };
+            }) ?? [];
+          setStories(stories);
+          setLoading(false);
+        })
+        .catch((error) => {
+          if ((error as Error).name !== "AbortError") {
+            console.error("Error:", error);
+            setLoading(false);
+          }
+        });
+        return;
     }
 
     fetch(`${trieveBaseURL}/chunk/search`, {
