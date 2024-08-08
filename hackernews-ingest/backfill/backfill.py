33b86077f9582e8f8998176f0c56074ae0a3c834
cdxker <fdenzell@gmail.com>
2024-08-08 15:43:55 -0700

    feature: url as tag in tag_set

---
+++ hackernews-ingest/backfill/backfill.py
@@ -0,0 +1,21 @@
+import os
+import requests
+import redis
+print("backfill init")
+
+redis_host = os.getenv("REDIS_URL")
+
+# Connect to Redis
+redis_client = redis.Redis(host=redis_host)
+lastfinal = int(requests.get("https://hacker-news.firebaseio.com/v0/maxitem.json").text)
+
+for i in range(lastfinal - 100_000, lastfinal):
+    redis_client.lpush("tovisit", i)
+
+redis_client.lpush("tovisit", *requests.get("https://hacker-news.firebaseio.com/v0/updates.json").json()["items"])
+redis_client.lpush("tovisit", *requests.get("https://hacker-news.firebaseio.com/v0/askstories.json").json())
+redis_client.lpush("tovisit", *requests.get("https://hacker-news.firebaseio.com/v0/showstories.json").json())
+redis_client.lpush("tovisit", *requests.get("https://hacker-news.firebaseio.com/v0/beststories.json").json())
+redis_client.lpush("tovisit", *requests.get("https://hacker-news.firebaseio.com/v0/topstories.json").json())
+redis_client.lpush("tovisit", *requests.get("https://hacker-news.firebaseio.com/v0/newstories.json").json())
+print("Queued backfill successfully")
