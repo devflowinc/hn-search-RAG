2226145070ab52a3d6eda906a4165d6b66fe65c8
skeptrune <nick.k@trieve.ai>
2024-08-09 16:29:32 -0700

    feature: properly handle fetching count for a given query

--- hackernews-ingest/comments_to_group/index.ts
+++ hackernews-ingest/comments_to_group/index.ts
@@ -1,16 +1,20 @@
-import { createClient } from 'redis';
+import { createClient } from "redis";
 
 async function getItem(i: Number) {
-    return await (await fetch(`https://hacker-news.firebaseio.com/v0/item/${i}.json?print=pretty`)).json()
+  return await (
+    await fetch(
+      `https://hacker-news.firebaseio.com/v0/item/${i}.json?print=pretty`
+    )
+  ).json();
 }
 
 async function getRootParent(i: Number) {
-    let it = await getItem(i);
-    if (it.parent) {
-        return getRootParent(it.parent)
-    }
+  let it = await getItem(i);
+  if (it.parent) {
+    return getRootParent(it.parent);
+  }
 
-    return it
+  return it;
 }
 
 let apiHost = process.env.TRIEVE_API_HOST;
@@ -19,57 +23,57 @@ let datasetid = process.env.DATASET_ID;
 let redisUri = process.env.REDIS_URI;
 
 async function upsertGroup(parent: Number) {
-    console.log("Creating group ", parent);
-    let resp = await fetch(`${apiHost}/api/chunk_group`, {
-        headers: {
-            "Content-Type": "application/json",
-            "TR-Dataset": datasetid,
-            "Authorization": apiKey,
-        },
-        method: "POST",
-        body: JSON.stringify({
-            tracking_id: `${parent}`,
-            upsert_by_tracking_id: true,
-            name: `${parent}`
-        })
-    });
-    console.log(await resp.text());
+  console.log("Creating group ", parent);
+  let resp = await fetch(`${apiHost}/api/chunk_group`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "TR-Dataset": datasetid,
+      Authorization: apiKey,
+    } as HeadersInit,
+    body: JSON.stringify({
+      tracking_id: `${parent}`,
+      upsert_by_tracking_id: true,
+      name: `${parent}`,
+    }),
+  });
+  console.log(await resp.text());
 }
 
 async function addToGroup(parent: Number, child: Number) {
-    console.log("Adding ", parent, " to group ", child);
-    let resp = await fetch(`${apiHost}/api/chunk_group/tracking_id/${parent}`, {
-        headers: {
-            "Content-Type": "application/json",
-            "TR-Dataset": datasetid,
-            "Authorization": apiKey,
-        },
-        method: "POST",
-        body: JSON.stringify({
-            chunk_tracking_id: `${child}`,
-        })
-    });
-    console.log(await resp.text());
+  console.log("Adding ", parent, " to group ", child);
+  let resp = await fetch(`${apiHost}/api/chunk_group/tracking_id/${parent}`, {
+    headers: {
+      "Content-Type": "application/json",
+      "TR-Dataset": datasetid,
+      Authorization: apiKey,
+    } as HeadersInit,
+    method: "POST",
+    body: JSON.stringify({
+      chunk_tracking_id: `${child}`,
+    }),
+  });
+  console.log(await resp.text());
 }
 
-
-
 const client = await createClient({
-    url: redisUri
-}).on('error', err => console.log('Redis Client Error', err)).connect();
+  url: redisUri,
+})
+  .on("error", (err: any) => console.log("Redis Client Error", err))
+  .connect();
 
 while (true) {
-    let currentId = await client.BRPOP("comments_to_visit", 0.0);
-    currentId = currentId.element;
+  let currentId: any = await client.BRPOP("comments_to_visit", 0.0);
+  currentId = currentId.element;
 
-    console.log("processing", currentId);
-    let item = await getItem(currentId);
+  console.log("processing", currentId);
+  let item = await getItem(currentId);
 
-    if (item && item.type && item.type == "comment") {
-        let p = await getRootParent(currentId);
-        await upsertGroup(p.id);
-        await addToGroup(p.id, item.id);
-    }
+  if (item && item.type && item.type == "comment") {
+    let p = await getRootParent(currentId);
+    await upsertGroup(p.id);
+    await addToGroup(p.id, item.id);
+  }
 }
 
 await client.disconnect();
