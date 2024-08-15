c0bb2a21aef942432358683cf68a29860ba66eb1
cdxker <fdenzell@gmail.com>
2024-08-14 19:34:22 -0700

    feature: backfill less values at once

--- hackernews-ingest/backfill/backfill.py
+++ hackernews-ingest/backfill/backfill.py
@@ -9,7 +9,7 @@ redis_host = os.getenv("REDIS_URL")
 redis_client = redis.Redis(host=redis_host)
 lastfinal = int(requests.get("https://hacker-news.firebaseio.com/v0/maxitem.json").text)
 
-for i in range(lastfinal - 100_000, lastfinal):
+for i in range(lastfinal - 10_000, lastfinal):
     redis_client.lpush("tovisit", i)
 
 redis_client.lpush("tovisit", *requests.get("https://hacker-news.firebaseio.com/v0/updates.json").json()["items"])
