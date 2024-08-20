1e7fb798446953d70f56e20fe737eabe6f24418c
Drew Harris <harrisd@smu.edu>
2024-08-20 11:18:53 -0500

    feat: tailwind

--- actix-frontend/build.rs
+++ actix-frontend/build.rs
@@ -1,3 +1,17 @@
-fn main() {
+use std::{io, process::Command};
+
+fn main() -> Result<(), io::Error> {
+    let output = Command::new("npx")
+        .arg("tailwindcss")
+        .arg("-i")
+        .arg("./static/in.css")
+        .arg("-o")
+        .arg("./static/output.css")
+        .output()?;
+
+    // Stream output
+    println!("{}", String::from_utf8_lossy(&output.stdout));
+
     minijinja_embed::embed_templates!("src/templates");
+    Ok(())
 }
---
+++ actix-frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/templates/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
