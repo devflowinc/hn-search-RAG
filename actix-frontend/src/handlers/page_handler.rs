626d3d507a5ab3f61c006b83f1fdf3995cde1093
Drew Harris <harrisd@smu.edu>
2024-08-20 16:07:13 -0500

    feat: refactor form system to always use get

--- actix-frontend/src/handlers/page_handler.rs
+++ actix-frontend/src/handlers/page_handler.rs
@@ -1,13 +1,36 @@
-use actix_web::{get, post, HttpResponse};
+use actix_web::{get, web, HttpResponse};
 use minijinja::context;
 use serde::Deserialize;
 
 use crate::Templates;
 
+#[derive(Deserialize, Debug)]
+struct SearchFormData {
+    pub query: String,
+    pub post_type: String,
+    pub sort_by: String,
+    pub date_range: String,
+    pub search_type: String,
+}
+
 #[get("/")]
-pub async fn homepage(templates: Templates<'_>) -> impl actix_web::Responder {
+pub async fn homepage(
+    templates: Templates<'_>,
+    form_data: Option<web::Query<SearchFormData>>,
+) -> impl actix_web::Responder {
     let templ = templates.get_template("homepage.html").unwrap();
-    let response_body = templ.render(context! {}).unwrap();
+
+    let form_data = form_data;
+
+    let response_body = match form_data {
+        Some(form_data) => templ
+            .render(context! {
+                query => form_data.query
+            })
+            .unwrap(),
+        None => templ.render(context! {}).unwrap(),
+    };
+
     HttpResponse::Ok().body(response_body)
 }
 
@@ -22,31 +45,5 @@ pub async fn about(templates: Templates<'_>) -> impl actix_web::Responder {
 pub async fn help(templates: Templates<'_>) -> impl actix_web::Responder {
     let templ = templates.get_template("help.html").unwrap();
     let response_body = templ.render(context! {}).unwrap();
-    HttpResponse::Ok().body(response_body)
-}
-
-#[derive(Deserialize, Debug)]
-struct SearchFormData {
-    pub query: String,
-    pub post_type: String,
-    pub sort_by: String,
-    pub date_range: String,
-    pub search_type: String,
-}
-
-#[post("/")]
-pub async fn post(
-    templates: Templates<'_>,
-    form_data: actix_web::web::Form<SearchFormData>,
-) -> impl actix_web::Responder {
-    let templ = templates.get_template("homepage.html").unwrap();
-
-    println!("Form data: {:?}", form_data);
-
-    let response_body = templ
-        .render(context! {
-            query => form_data.query
-        })
-        .unwrap();
     HttpResponse::Ok().body(response_body)
 }
