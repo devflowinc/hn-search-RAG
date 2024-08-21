badfed7fe9cbe40847e3352b0418d4a034aeee04
Drew Harris <harrisd@smu.edu>
2024-08-21 15:36:25 -0500

    styling: result divs and time_ago

---
+++ actix-frontend/src/format_time.rs
@@ -0,0 +1,42 @@
+use chrono::{DateTime, Utc};
+
+pub fn time_ago(timestamp: i64) -> String {
+    let now: DateTime<Utc> = Utc::now();
+    let datetime: DateTime<Utc> = DateTime::<Utc>::from(
+        std::time::SystemTime::UNIX_EPOCH + std::time::Duration::from_secs(timestamp as u64),
+    );
+
+    let duration = now - datetime;
+
+    if duration.num_seconds() < 60 {
+        format!(
+            "{} second{} ago",
+            duration.num_seconds(),
+            if duration.num_seconds() != 1 { "s" } else { "" }
+        )
+    } else if duration.num_minutes() < 60 {
+        format!(
+            "{} minute{} ago",
+            duration.num_minutes(),
+            if duration.num_minutes() != 1 { "s" } else { "" }
+        )
+    } else if duration.num_hours() < 24 {
+        format!(
+            "{} hour{} ago",
+            duration.num_hours(),
+            if duration.num_hours() != 1 { "s" } else { "" }
+        )
+    } else if duration.num_days() < 30 {
+        format!(
+            "{} day{} ago",
+            duration.num_days(),
+            if duration.num_days() != 1 { "s" } else { "" }
+        )
+    } else if duration.num_days() < 365 {
+        let months = (duration.num_days() as f64 / 30.4375).floor() as i64;
+        format!("{} month{} ago", months, if months != 1 { "s" } else { "" })
+    } else {
+        let years = (duration.num_days() as f64 / 365.2425).floor() as i64;
+        format!("{} year{} ago", years, if years != 1 { "s" } else { "" })
+    }
+}
